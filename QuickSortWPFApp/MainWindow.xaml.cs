44c935f22bc48ef5fc4c320b3d765a6a441ef596
gabee1987 <gabee1987@gmail.com>
2018-01-05 19:10:09 +0100

    created WPF window with all elements, created random generator feature

---
+++ QuickSortWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuickSortWPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        NumberManager numberManager = NumberManager.Instance;
+        MainWindow mw = (MainWindow)Application.Current.MainWindow;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            numberManager.GenerateNumbersInGivenRange(numberManager.Range);
+
+        }
+
+        private void NumbersToGenerateSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            numberManager.Range = (int)NumbersToGenerateSlider.Value;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            numberManager.Range = (int)NumbersToGenerateSlider.Value;
+            NumbersToSortListView.ItemsSource = numberManager.NumbersToSort;
+            SortedResultListView.ItemsSource = numberManager.SortedResult;
+        }
+    }
+}
---
+++ QuickSortWPFApp/NumberManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSortWPFApp
+{
+    class NumberManager
+    {
+        private ObservableCollection<int> numbersToSort = new ObservableCollection<int>();
+        private ObservableCollection<int> sortedResult = new ObservableCollection<int>();
+        private int range;
+
+        internal ObservableCollection<int> NumbersToSort { get => numbersToSort; set => numbersToSort = value; }
+        internal ObservableCollection<int> SortedResult { get => sortedResult; set => sortedResult = value; }
+        public int Range { get => range; set => range = value; }
+
+        private static NumberManager instance = null;
+        private NumberManager() { }
+        public static NumberManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new NumberManager();
+                }
+                return instance;
+            }
+        }
+
+
+        public void GenerateNumbersInGivenRange(int range)
+        {
+            Random random = new Random();
+            numbersToSort.Clear();
+
+            for (int i = 1; i < range + 1; i++)
+            {
+                numbersToSort.Add(random.Next(range));
+            }
+        }
+
+    }
+}
